fb30806e2b0e79b09eca6571c73ee35a80cd3a5d
Gin Suarez <ginnsuarez@gmail.com>
2022-07-24 13:04:29 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn react
+          {/* Hi there! New Changes, even newer, newer still */}
         </a>
       </header>
     </div>
